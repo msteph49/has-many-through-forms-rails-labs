13e9ff0d0f21f3400a02906e96a69b6ea1fafca6
Michael Stephens <mdstephens26@yahoo.com>
2021-07-18 17:11:21 -0400

    commit tree 68240f9a8cda9b9c8b2352890146a91303cb8412

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category|
+      if category[:name].present?
+        self.categories << Category.find_or_create_by(name: category[:name])
+      end
+    end
+  end
+  
 end
